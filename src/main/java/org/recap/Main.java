262ad6523b8767596bd1c6cbaaaef2f1922b936d
NSSuresh11 <nssuresh.subramanian@gmail.com>
2020-05-18 12:22:25 +0530

    Tech Upgrade for Phase4-SCSB-Gateway

--- src/main/java/org/recap/Main.java
+++ src/main/java/org/recap/Main.java
@@ -2,10 +2,12 @@ package org.recap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * The main class is used to launch the spring boot application.
  */
+@PropertySource("classpath:application.properties")
 @SpringBootApplication
 public class Main {
 
