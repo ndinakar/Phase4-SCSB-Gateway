552a41bea6f15eb0fc5f302e566857f9b6bb68a3
rmaheswaran <rmaheswaran@ciber.com>
2020-06-23 14:36:29 +0530

    Logstash and zipkin libraries addition

--- src/main/java/org/recap/Main.java
+++ src/main/java/org/recap/Main.java
@@ -2,7 +2,10 @@ package org.recap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+
+import brave.sampler.Sampler;
 
 /**
  * The main class is used to launch the spring boot application.
@@ -18,5 +21,10 @@ public class Main {
      */
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+      
+    @Bean
+    public Sampler defaultSampler() {
+          return Sampler.ALWAYS_SAMPLE;
     }
 }
