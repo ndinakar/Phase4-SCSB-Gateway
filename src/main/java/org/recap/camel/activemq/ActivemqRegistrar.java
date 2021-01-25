199a7c3a7dbfb42102a2072e040c634df160fb54
rajeshbabuk <rajeshbabu.k@htcindia.com>
2021-01-25 13:13:17 +0530

    Refactor code.

--- src/main/java/org/recap/camel/activemq/ActivemqRegistrar.java
+++ src/main/java/org/recap/camel/activemq/ActivemqRegistrar.java
@@ -3,6 +3,7 @@ package org.recap.camel.activemq;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.activemq.ActiveMQComponent;
+import org.recap.RecapCommonConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -30,7 +31,7 @@ public class ActivemqRegistrar {
         ActiveMQComponent activeMQComponent = new ActiveMQComponent();
         activeMQComponent.setConnectionFactory(connectionFactory);
         activeMQComponent.setTrustAllPackages(true);
-        camelContext.addComponent("scsbactivemq", activeMQComponent);
+        camelContext.addComponent(RecapCommonConstants.SCSB_ACTIVEMQ_COMPONENT_NAME, activeMQComponent);
     }
 
 }
