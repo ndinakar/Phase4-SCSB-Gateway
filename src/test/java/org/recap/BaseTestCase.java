41722a379870011943ebb4074d41d4101ed23a4e
ndinakar <dnaramala@ciber.com>
2022-03-01 12:25:27 +0530

    chagnes for JUNIT

--- src/test/java/org/recap/BaseTestCase.java
+++ src/test/java/org/recap/BaseTestCase.java
@@ -1,16 +1,18 @@
 package org.recap;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
-@ExtendWith(MockitoExtension.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Main.class)
-@Rollback(false)
+@WebAppConfiguration
+@Transactional
+@Rollback()
 public class BaseTestCase {
 
 	@Test
