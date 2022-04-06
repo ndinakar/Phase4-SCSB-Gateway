f7249b1e697079913e855119eaf95cd0a7b25ad6
ndinakar <dnaramala@ciber.com>
2022-04-06 11:18:52 +0530

    log added for Init Binder

--- src/main/java/org/recap/BinderControllerAdvice.java
+++ src/main/java/org/recap/BinderControllerAdvice.java
@@ -1,5 +1,8 @@
 package org.recap;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.WebDataBinder;
@@ -8,12 +11,15 @@ import org.springframework.web.bind.annotation.InitBinder;
 
 @ControllerAdvice
 @Order(Ordered.LOWEST_PRECEDENCE)
+@Slf4j
 public class BinderControllerAdvice {
+    private static final Logger logger = LoggerFactory.getLogger(BinderControllerAdvice.class);
 
     @InitBinder
     public void setAllowedFields(WebDataBinder dataBinder) {
-         String[] denylist = new String[]{"class.*", "Class.*", "*.class.*", "*.Class.*"};
-         dataBinder.setDisallowedFields(denylist);
+        logger.info("Init BInder is Called");
+        String[] denylist = new String[]{"class.*", "Class.*", "*.class.*", "*.Class.*"};
+        dataBinder.setDisallowedFields(denylist);
     }
 
 }
